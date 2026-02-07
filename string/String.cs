f7a27f667ba48c728b431fd2d14f8ec6e47748cb
thaiannguyen-05 <thaiandev05@gmail.com>
2026-02-07 23:37:30 +0700

    text

---
+++ string/String.cs
@@ -0,0 +1,15 @@
+﻿// normal log
+string greeting = "                  HELLO WORD";
+Console.WriteLine($"{greeting}");
+
+// trim only head
+string trimmedGreeting = greeting.TrimStart();
+Console.WriteLine($"{trimmedGreeting}");
+
+// trim only end
+trimmedGreeting = greeting.TrimEnd();
+Console.WriteLine($"{trimmedGreeting}");
+
+// trim between 
+trimmedGreeting = greeting.Trim();
+Console.WriteLine($"{trimmedGreeting}");
