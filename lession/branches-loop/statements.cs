85bb2ca4b165905dc5cf9fb6e2704a5f6569137c
thaiannguyen-05 <thaiandev05@gmail.com>
2026-02-08 09:30:43 +0700

    statement + loop

---
+++ lession/branches-loop/statements.cs
@@ -0,0 +1,15 @@
+// int a = 5;
+// int b = 6;
+
+// // if(Boolean) 
+// //     // do something
+// // else 
+// //     // do something
+
+
+// Console.WriteLine((a > b) ? "OK" : "NO");
+
+// for (char column = 'a'; column < 'k'; column++)
+// {
+//     Console.WriteLine($"The column is {column}");
+// }
